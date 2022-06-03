63206c17d4b7b6a9b25d7d7ff9869c18996a5317
Suprememajor <ndipnobertetta@gmail.com>
2022-06-03 02:10:04 +0100

    task 4-puts.c redo 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
 *Entry point of the program
 */
-int main(void)
+int main (void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
