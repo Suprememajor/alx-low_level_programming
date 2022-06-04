06f4664ea7259104bf11fa429c8101660018700d
Suprememajor <ndipnobertetta@gmail.com>
2022-06-04 02:17:11 +0100

    task 0-positive_or_negative.c redo2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -22,4 +27,3 @@ int main(void)
 	}
 	return (0);
 }
-
