b337e52c0f6aa0a8880d45f458d0125c0d724dcb
Suprememajor <ndipnobertetta@gmail.com>
2022-06-04 03:27:08 +0100

    task 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
