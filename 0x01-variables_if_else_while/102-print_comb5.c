ed207ac2631e4a377ac7dce8e4afb10c8a5df588
Suprememajor <ndipnobertetta@gmail.com>
2022-06-04 03:57:53 +0100

    task102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e, g, h, op1, op2;
+
+	i = e = g = h = 48;
+	for (; h < 58; h++)
+	{		
+		for (g = 48; g < 58; g++)
+		{			
+			for (e = 48; e < 58; e++)
+			{
+				for (i = 48; i < 58; i++)
+				{
+					op1 = (h * 10) + g;
+					op2 = (e * 10) + i;
+					if (op1 < op2)
+					{
+						putchar(h);
+						putchar(g);
+						putchar(' ');
+						putchar(e);
+						putchar(i);
+						if (h == 57 && g == 56 && e == 57 && i == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
