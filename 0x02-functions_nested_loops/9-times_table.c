9b66177787a0c08bb3752e824115890809676a42
Suprememajor <ndipnobertetta@gmail.com>
2022-06-10 22:00:02 +0100

    Task 9-times_table.c redo

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,19 +2,31 @@
 /**
  * times_table - prints times table
  */
- void times_table(void)
- {
- 	int i, j;
- 	
- 	for (i = 0; i <= 9; i++)
- 	{
- 		for (j = 0; j <= 9; j++)
- 		{
- 			_putchar((i * j) + 48);
- 			if (j == 9)
- 				continue;
- 			_putchar(44);
- 		}
- 		_putchar('\n');
- 	}
- }
+void times_table(void)
+{
+	int i, j, val;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j != 0)
+				_putchar(32);
+			val = i * j;
+			if (val <= 9)
+			{
+				_putchar(32);
+				_putchar(val + 48);
+			}
+			else
+			{
+				_putchar((val / 10) + 48);
+				_putchar((val % 10) + 48);
+			}
+			if (j == 9)
+				continue;
+			_putchar(44);
+		}
+		_putchar('\n');
+	}
+}
