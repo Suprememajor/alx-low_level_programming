c1d55e215e4314376c2277704de283fa5b556642
Suprememajor <ndipnobertetta@gmail.com>
2022-06-10 21:24:22 +0100

    Task 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - prints and returns last digit of an integer
+ *
+ * Return: 
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 48; i <= 50; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if ((i == 50) && (j > 51))
+			{
+				break;
+			}
+			for (k = 48; k <= 53; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
