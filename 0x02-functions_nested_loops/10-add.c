a538eef97b2ce229ba96125797ffd028fed733aa
Suprememajor <ndipnobertetta@gmail.com>
2022-06-10 22:21:32 +0100

    Task 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Returns the sum of two numbers
+ *
+ *@m: first number to add
+ *@n: second number to add
+ *
+ * Return: The sum of the numbers
+ */
+int add(int m, int n)
+{
+	int sum;
+	
+	return (m + n);
+}
