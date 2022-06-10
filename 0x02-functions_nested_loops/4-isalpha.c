d6f94646a884a028b54450af452645bbbcfb6980
Suprememajor <ndipnobertetta@gmail.com>
2022-06-10 20:05:03 +0100

    Task 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Tells if a character is alphabetic or not
+ *
+ *@c: The character in ASCII code
+ *
+ * Return: 1 if alphabetic, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123 || c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
