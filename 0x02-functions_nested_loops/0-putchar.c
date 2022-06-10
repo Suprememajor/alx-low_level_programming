26e2e1bea312756e1255b58f8435676b02f96d24
Suprememajor <ndipnobertetta@gmail.com>
2022-06-10 19:30:05 +0100

    Task 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
+}
