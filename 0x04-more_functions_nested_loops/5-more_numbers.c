87f2f5bffde7b122e2b09ed08c589890e68efe27
Suprememajor <ndipnobertetta@gmail.com>
2022-06-11 10:10:12 +0100

    Task 5-more_numbers.c redo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,22 +6,15 @@
  */
 void more_numbers(void)
 {
-	int i, j, k;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 48; j < 50; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			for (k = 48; k < 58; k++)
-			{
-				if (j == 48)
-					_putchar(k);
-				else if (j == 49 && k < 53)
-				{
-					_putchar(j);
-					_putchar(k);
-				}
-			}
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
