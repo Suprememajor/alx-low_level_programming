fa55146965119d547be4e3ce6b4fbe3944626a5c
Suprememajor <ndipnobertetta@gmail.com>
2022-06-11 09:33:40 +0100

    Task 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - Tells if a character is a digit or not
+ *
+ *@c: The character in ASCII code
+ *
+ * Return: 1 if it is a digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
