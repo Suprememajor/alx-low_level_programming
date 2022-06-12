262dd401a7fb3cf17feee14b23f83ef86e84ad9e
Suprememajor <ndipnobertetta@gmail.com>
2022-06-12 04:32:46 +0100

    Task 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle of variable size
+ *
+ *@size: variable that determines the size of the line
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < (size - i - 1); j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < (i +1); k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
