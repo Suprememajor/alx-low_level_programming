0f1a4a4be6acf4480519edd13499b758728086a6
Suprememajor <ndipnobertetta@gmail.com>
2022-06-12 04:14:12 +0100

    Task 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - print a square of variable size
+ *
+ *@size: variable that determines the size of the line
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (n > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
