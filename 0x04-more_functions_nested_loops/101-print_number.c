b84626ce73acf7dafb275367c48568c6fa04e61f
Suprememajor <ndipnobertetta@gmail.com>
2022-06-12 05:53:11 +0100

    Task 101-print_number.c redo 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,27 @@ void print_number(int n)
 	int reverse, last_digit;
 
 	reverse = 0;
-	while (n > 0)
+	if (n == 0)
+		_putchar(48);
+	else
 	{
-		last_digit = n % 10;
-		reverse = (reverse * 10) + (last_digit);
-		n /= 10;
-	}
-	while (reverse > 0)
-	{
-		last_digit = reverse % 10;
-		_putchar(last_digit + 48);
-		reverse /= 10;
+		if (n < 0)
+		{
+			_putchar('-');
+			n *= -1;
+		}
+		while (n > 0)
+		{
+			last_digit = n % 10;
+			reverse = (reverse * 10) + (last_digit);
+			n /= 10;
+		}
+		while (reverse > 0)
+		{
+			last_digit = reverse % 10;
+			_putchar(last_digit + 48);
+			reverse /= 10;
+		}
 	}
 	_putchar('\n');
 }
