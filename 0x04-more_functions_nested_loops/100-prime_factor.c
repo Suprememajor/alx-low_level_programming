7676eacff0454375405076bb2206b3ed7ca60b4f
Suprememajor <ndipnobertetta@gmail.com>
2022-06-12 05:30:48 +0100

    Task 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n, i;
+
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			i--;
+		}
+	}
+	printf("%ld\n", i);
+}
